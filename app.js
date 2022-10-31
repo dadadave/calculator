576c5fe01a3d3723fefdd89a95c0ed7c4b9082fa
dadadave <mpele.jusue@ictuniversity.edu.cm>
2022-10-31 08:41:42 +0100

    a simple calculator dine in html css and some js
    a simple calculator dine in html css
    and some js

---
+++ app.js
@@ -0,0 +1,34 @@
+//selector
+const output = document.querySelector(".output");
+const result = document.querySelector(".result");
+const keys = document.querySelectorAll("button");
+
+//evenlistener
+keys.forEach(key=>{
+    key.addEventListener("click",calculate);
+});
+
+function calculate(){
+    let buttonText = this.innerText;
+    if(buttonText==="AC"){
+        output.innerText = "";
+        result.innerText = "0";
+        return;
+    }
+    if(buttonText==="DEL"){
+        output.textContent = output.textContent.substring(0,output.textContent.length-1);
+        return;
+    }
+    if(buttonText==="="){
+        result.innerText = eval(output.innerText);
+        result.style.animation = "big 0.5s ease-in-out";
+        result.style.animation = "small 0.5s ease-in-out";
+        result.style.animation = "forward";
+        output.style.animation = "forward";
+    }
+    else{
+        output.textContent += buttonText;
+        return;
+    }
+
+}
